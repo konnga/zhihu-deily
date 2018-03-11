4fd5ff66e86ccc2d46f64c50c3b5926fdfabd29e
‘wangjiacheng’ <wjc402402@163.com>
2018-03-11 23:00:55 +0800

    v0.0.1 初始化vue-zhihu项目

---
+++ src/conf/bootstrap.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+
+export default () => {
+  new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>',
+  });
+};
